bb5984e9557c2da1fcafeae1d9997d8c716c330c
sourdubu <90190948+sourdubu@users.noreply.github.com>
2022-07-27 21:06:50 +0800

    Add files via upload

---
+++ oo.sh
@@ -0,0 +1,17 @@
+#!/system/bin/sh
+
+# ECHO command
+# echo hello world!
+
+# VARIABLES
+# Uppedcase by convention
+# Letters, numbers, underscores
+# CHOICE= Soviet
+# echo "Your choice is $CHOICE"
+
+# USER INPUT                                               read -p "Please input your choice: " CHOICE
+
+read -p "Please input your choice:"
+
+echo "so your choice is ${CHOICE}"
+
